071e38efb6f6676370b5484dc586c4ec25c1f68e
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-27 09:49:32 +0500

    post application

---
+++ app.js
@@ -0,0 +1,6 @@
+var  title = document.getElementsByClassName(".title")
+title.className="title"
+var discription = document.getElementsById("textarea")
+discription.className="discription"
+// var discription = document.createElement("textarea")
+// var outerDiv = document.getElementById("outerDiv")
