f5454e6af821185ac924ca750ed4a7cb7726b660
Tayyabakhatri <Tayyabakhatri225@gmail.com>
2024-08-28 16:34:32 +0500

    functionalities added

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-var  title = document.getElementsByClassName(".title")
-title.className="title"
-function titleName(){
+var title = document.getElementsByClassName(".title")
+title.className = "title"
+function titleName() {
+
     var title = document.getElementById("name")
     var name = title.value
-    console.log(name)
-    title.value=""
+    var discription = document.getElementById("textarea");
+    var input = discription.value
+    if (name == "" || input == "") {
+        alert("Title and discription are required field")
+    } else {
+        var receivedTitle= document.getElementById("receivedTitle")
+        receivedTitle.innerHTML=name
+        var received = document.getElementById("receivedText")
+        received.innerHTML = input
+
+        title.value = ""
+        discription.value = ""
+    }
+
+}
+function discard() {
+    var received = document.getElementById("receivedText")
+    received.innerHTML = ""
+    var receivedTitle= document.getElementById("receivedTitle")
+    receivedTitle.innerHTML=""
 }
 
